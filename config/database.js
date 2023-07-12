65126b61a2e77d15d1420ce768508593c39e795a
alabalagithub <139351716+alabalagithub@users.noreply.github.com>
2023-07-12 16:37:49 +0300

    Update database.js123123

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      url: env("DATABASE_URL"),
+      url: "postgres://admin:jpqyTWeFy9myRVn1jdffZPzNUm1iTuFL@dpg-cin9evp5rnur6sd92s2g-a.frankfurt-postgres.render.com/store_zew5",
       ssl: {
         rejectUnauthorized: false,
       },
