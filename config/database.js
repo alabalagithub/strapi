96ae09fcdcfb4ae40f86a7ee8d82e10d77643a8d
alabalagithub <139351716+alabalagithub@users.noreply.github.com>
2023-07-12 16:44:59 +0300

    Update database.js112323

--- config/database.js
+++ config/database.js
@@ -2,7 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      url: "postgres://admin:jpqyTWeFy9myRVn1jdffZPzNUm1iTuFL@dpg-cin9evp5rnur6sd92s2g-a.frankfurt-postgres.render.com/store_zew5",
+      host: "dpg-cin9evp5rnur6sd92s2g-a.frankfurt-postgres.render.com",
+      port: 5432,
+      database: "store_zew5",
+      username: "admin",
+      password: "jpqyTWeFy9myRVn1jdffZPzNUm1iTuFL",
       ssl: {
         rejectUnauthorized: false,
       },
